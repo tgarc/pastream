f93cb4963ed40b849bb99448280653c4c70417e6
tdos.apone <toemossgarcia@gmail.com>
2017-05-27 13:28:43 -0500

    updates for proper source code deployment

--- setup.py
+++ setup.py
@@ -1,36 +1,41 @@
+import os
 from setuptools import setup, find_packages
 
 __version__ = 'unknown'
 
 # "import" __version__
-for line in open('pastream.py'):
+dirname = os.path.dirname(__file__)
+for line in open(os.path.join(dirname, 'pastream.py')):
     if line.startswith('__version__'):
         exec(line)
         break
 
 setup(name='pastream',
       version=__version__,
+      py_modules=['pastream'],
       author='Thomas J. Garcia',
       url='http://github.com/tgarc/pastream',
+      platforms='any',
+      license='MIT',
       author_email='toemossgarcia@gmail.com',
-      long_description=open('README.rst').read(),
+      long_description=open(os.path.join(dirname, 'README.rst')).read(),
       entry_points={'console_scripts': ['pastream = pastream:_main']},
       cffi_modules=["build_pastream.py:ffibuilder"],
       install_requires=[
           'cffi>=1.0.0',
           'soundfile>=0.9.0',
           'sounddevice>=0.3.7',
-          ],
+      ],
       setup_requires=['CFFI>=1.4.0'],
       tests_require=['pytest>=3.0', 'numpy'],
       extras_require={'examples': ['numpy', 'matplotlib']},
-      py_modules=['pastream'],
-      include_package_data=True,
       classifiers=[
+          'License :: OSI Approved :: MIT License',
           'Operating System :: OS Independent',
           'Programming Language :: Python',
           'Programming Language :: Python :: 2',
           'Programming Language :: Python :: 3',
           'Topic :: Multimedia :: Sound/Audio'
-          ],
+      ],
+      include_package_data=True,
       zip_safe=False)
