fb7127ee7af391000e8a29b057ac3cd183c886fc
tdos.apone <toemossgarcia@gmail.com>
2017-08-09 01:02:21 -0500

    use the new pa_ringbuffer on pip
    - no need to vendor pa_ringbuffer anymore, it's available on pip!

--- build_pastream.py
+++ build_pastream.py
@@ -1,10 +1,5 @@
 from cffi import FFI
-import platform
-
-if platform.system() == 'Darwin':
-    ring_buffer_size_t = 'int32_t'
-else:
-    ring_buffer_size_t = 'long'
+import pa_ringbuffer
 
 ffibuilder = FFI()
 ffibuilder.cdef(r"""
@@ -23,30 +18,9 @@ typedef unsigned long PaStreamCallbackFlags;
 #define paOutputOverflow  0x00000008
 #define paPrimingOutput   0x00000010
 
-/* From pa_ringbuffer.h: */
+""")
 
-typedef %(ring_buffer_size_t)s ring_buffer_size_t;
-typedef struct PaUtilRingBuffer
-{
-    ring_buffer_size_t  bufferSize;
-    volatile ring_buffer_size_t writeIndex;
-    volatile ring_buffer_size_t readIndex;
-    ring_buffer_size_t elementSizeBytes;
-    char* buffer;
-    ...;
-} PaUtilRingBuffer;
-
-ring_buffer_size_t PaUtil_InitializeRingBuffer(PaUtilRingBuffer* rbuf, ring_buffer_size_t elementSizeBytes, ring_buffer_size_t elementCount, void* dataPtr);
-void PaUtil_FlushRingBuffer(PaUtilRingBuffer* rbuf);
-ring_buffer_size_t PaUtil_GetRingBufferWriteAvailable(const PaUtilRingBuffer* rbuf);
-ring_buffer_size_t PaUtil_GetRingBufferReadAvailable(const PaUtilRingBuffer* rbuf);
-ring_buffer_size_t PaUtil_WriteRingBuffer(PaUtilRingBuffer* rbuf, const void* data, ring_buffer_size_t elementCount);
-ring_buffer_size_t PaUtil_ReadRingBuffer(PaUtilRingBuffer* rbuf, void* data, ring_buffer_size_t elementCount);
-ring_buffer_size_t PaUtil_GetRingBufferWriteRegions(PaUtilRingBuffer* rbuf, ring_buffer_size_t elementCount, void** dataPtr1, ring_buffer_size_t* sizePtr1, void** dataPtr2, ring_buffer_size_t* sizePtr2);
-ring_buffer_size_t PaUtil_AdvanceRingBufferWriteIndex(PaUtilRingBuffer* rbuf, ring_buffer_size_t elementCount);
-ring_buffer_size_t PaUtil_GetRingBufferReadRegions(PaUtilRingBuffer* rbuf, ring_buffer_size_t elementCount, void** dataPtr1, ring_buffer_size_t* sizePtr1, void** dataPtr2, ring_buffer_size_t* sizePtr2);
-ring_buffer_size_t PaUtil_AdvanceRingBufferReadIndex(PaUtilRingBuffer* rbuf, ring_buffer_size_t elementCount);
-""" % locals())
+ffibuilder.cdef(pa_ringbuffer.cdef())
 
 ffibuilder.cdef(open('src/py_pastream.h').read())
 
