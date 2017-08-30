df91c518a2788baca8b5e676bb99bbe8cb83c264
tdos.apone <toemossgarcia@gmail.com>
2017-08-30 09:52:51 -0500

    msvc doesn't support designated initializers
    replace designated initializer for msvc

--- src/py_pastream.h
+++ src/py_pastream.h
@@ -5,46 +5,52 @@ typedef struct Py_PaStream {
     PaStreamCallbackFlags status;
     PaStreamCallbackTimeInfo lastTime;
     int last_callback;
-    unsigned char __autoframes;   // Internal use only
+    unsigned long long frame_count;        // Number of frames successfully processed
     long long frames;             // Number of frames to play/record (-1 means infinite)
     long pad;                     // Number of zero frames to pad the playback with
                                   // (< 0 means to pad playback whenever buffer is empty)
+    unsigned long offset;         // Number of frames to skip from beginning of recordings
     unsigned long xruns;
     unsigned long inputOverflows, inputUnderflows;
     unsigned long outputOverflows, outputUnderflows;
-    unsigned long long frame_count;        // Number of frames successfully processed
-    unsigned long offset;         // Number of frames to skip from beginning of recordings
     PaUtilRingBuffer* rxbuff;     // Receive buffer
     PaUtilRingBuffer* txbuff;     // Transmit buffer
     char errorMsg[MAX_MESSAGE_LEN];  // Reserved for errors raised in the audio callback
+    unsigned char __autoframes;   // Internal use only
 } Py_PaStream;
 
+// :sigh: Visual Studio doesn't support designated initializors
 const Py_PaStream Py_PaStream_default = {
-    .last_callback = paContinue,
-    .status = 0,
-    .frame_count = 0,
-    .frames = -1,
-    .pad = 0,
-    .offset = 0,
-    .rxbuff = NULL,
-    .txbuff = NULL,
-    .xruns = 0,
-    .inputUnderflows = 0,
-    .inputOverflows = 0,
-    .outputUnderflows = 0,
-    .outputOverflows = 0,
-    .__autoframes = 0
+    0,
+    0,
+    paContinue,
+    0,
+    -1,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    NULL,
+    NULL,
+    '\0',
+    0
 };
 
-
-// call once for initialization
+// Call once for initialization
 void init_stream(Py_PaStream *stream);
 
 // Call before re-starting the stream
 void reset_stream(Py_PaStream *stream);
 
-int callback(const void* in_data, void* out_data,
-             unsigned long frame_count,
-             const PaStreamCallbackTimeInfo* timeInfo,
-             PaStreamCallbackFlags status,
-             void *user_data);
+int callback
+(
+    const void* in_data, 
+    void* out_data,
+    unsigned long frame_count,
+    const PaStreamCallbackTimeInfo* timeInfo,
+    PaStreamCallbackFlags status,
+    void *user_data
+);
