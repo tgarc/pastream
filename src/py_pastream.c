03f7b0df336b2d69ce86b333d3b01fcf732cf18e
Thomas Garcia <431388+tgarc@users.noreply.github.com>
2023-04-10 08:56:47 -0500

    tempCommit

--- src/py_pastream.c
+++ src/py_pastream.c
@@ -4,29 +4,17 @@
 #ifdef PYPA_DEBUG
 #include <stdio.h>
 #endif
+#include <stddef.h>
 
 
 void init_stream(Py_PaStream *stream)
 {
-    *stream = Py_PaStream_default;
-    reset_stream(stream);
+    memset((void *) stream, 0, sizeof(Py_PaStream));
 };
 
 void reset_stream(Py_PaStream *stream) {
     memset((void *) &stream->lastTime, 0, sizeof(PaStreamCallbackTimeInfo));
-
-    stream->last_callback = paContinue;
-    stream->status = 0;
-    stream->frame_count = 0;
-    stream->errorMsg[0] = '\0';
-    stream->xruns = 0;
-    stream->inputUnderflows = 0;
-    stream->inputOverflows = 0;
-    stream->outputUnderflows = 0;
-    stream->outputOverflows = 0;
-    if ( stream->_autoframes && stream->pad >= 0 )
-        stream->frames = -1;
-    stream->_autoframes = 0;
+    memset((void *) &stream->stats, 0, sizeof(Py_PaStreamStats));
 };
 
 int callback(
@@ -37,47 +25,25 @@ int callback(
     PaStreamCallbackFlags status,
     void *user_data)
 {
-    unsigned long offset = 0;
     ring_buffer_size_t frames_left = frame_count;
     ring_buffer_size_t oframes_left = frame_count;
     ring_buffer_size_t oframes, iframes, tempframes;
     Py_PaStream * stream = (Py_PaStream *) user_data;
-    long long frames = stream->frames;
-    long pad = stream->pad;
-    int i, j, k;
-    unsigned char temp, *ptemp;
-    unsigned char samplesize;
 
     if ( status & 0xF ) {
         if ( status & paInputUnderflow )
-            stream->inputUnderflows++;
+            stream->stats.inputUnderflows++;
         if ( status & paInputOverflow )
-            stream->inputOverflows++;
+            stream->stats.inputOverflows++;
         if ( status & paOutputUnderflow )
-            stream->outputUnderflows++;
+            stream->stats.outputOverflows++;
         if ( status & paOutputOverflow )
-            stream->outputOverflows++;
-        stream->xruns++;
-        stream->status |= status;
+            stream->stats.outputOverflows++;
+        stream->stats.xruns++;
     }
 
-    if ( frames >= 0 ) {
-        // exit point (1 of 2)
-        // We've surpassed frames: this is our last callback
-        if ( stream->frame_count + frames_left >= frames ) {
-            frames_left = frames - stream->frame_count;
-            stream->last_callback = paComplete;
-        }
-
-        // Calcuate how many output frames are left to read (minus any padding)
-        tempframes = frames - pad - stream->frame_count;
-        if ( pad >= 0 && frames_left >= tempframes ) {
-            oframes_left = tempframes > 0 ? tempframes : 0;
-        }
-    }
-
-    if ( out_data != NULL && (stream->txbuffer == NULL || stream->_autoframes) ) {
-        memset((unsigned char *) out_data, 0, frame_count*stream->txElementSize);
+    if ( out_data != NULL && stream->txbuffer == NULL ) {
+        memset((unsigned char *) out_data, 0, frame_count*stream->config.txElementSize);
     }
     else if ( stream->txbuffer != NULL ) {
         oframes = PaUtil_ReadRingBuffer(stream->txbuffer, out_data, oframes_left);
@@ -94,89 +60,22 @@ int callback(
             PaUtil_FlushRingBuffer(stream->txbuffer);
             PaUtil_AdvanceRingBufferWriteIndex(stream->txbuffer, tempframes);
             oframes += PaUtil_ReadRingBuffer(stream->txbuffer,
-                           (unsigned char *) out_data + oframes*stream->txElementSize,
+                           (unsigned char *) out_data + oframes*stream->config.txElementSize,
                            oframes_left - oframes);
         }
 
-        // Re-map data to requested output channels
-        if ( stream->txmapping != NULL ) {
-            samplesize = stream->txElementSize / stream->txchannels;
-            memcpy(stream->_mapping, stream->txmapping, stream->txchannels);
-
-            for ( i = 0 ; i < stream->txchannels ; i++ ) {
-                // inchannel = stream->_mapping[i];
-                // outchannel = i + 1;
-                if ( stream->_mapping[i] == i + 1 || stream->_mapping[i] == 0 )
-                    continue;
-
-                ptemp = (unsigned char *) out_data;
-                if ( i + 1 == stream->txchannels || stream->_mapping[stream->_mapping[i] - 1] == 0 ) {
-                    for ( j = 0 ; j < oframes ; j++, ptemp += stream->txElementSize ) {
-                        memcpy(ptemp + i * samplesize, ptemp + (stream->_mapping[i] - 1)*samplesize, samplesize);
-                    }
-                }
-                else {
-                    for ( j = 0 ; j < oframes ; j++, ptemp += stream->txElementSize ) {
-                        for ( k = 0 ; k < samplesize ; k++) {
-                            temp = ptemp[i * samplesize + k];
-                            ptemp[i * samplesize + k] = ptemp[(stream->_mapping[i] - 1)*samplesize + k];
-                            ptemp[(stream->_mapping[i] - 1)*samplesize + k] = temp;
-                        }
-                    }
-                }
-
-                for ( k = i + 1 ; k < stream->txchannels ; k++) {
-                    if (stream->_mapping[k] == i + 1)
-                        stream->_mapping[k] = stream->_mapping[i];
-                    else if (stream->_mapping[k] == stream->_mapping[i])
-                        stream->_mapping[k] = i + 1;
-                }
-            }
-            // Do channel zero out as a last step to avoid unnecessary swapping
-            for ( i = 0 ; i < stream->txchannels ; i++ ) {
-                if ( stream->_mapping[i] != 0 )
-                    continue;
-
-                ptemp = (unsigned char *) out_data + i * samplesize;
-                for ( j = 0 ; j < oframes ; j++, ptemp += stream->txElementSize ) {
-                    memset(ptemp, 0, samplesize);
-                }
-            }
+        // No matter what happens, fill the remainder of the output buffer with zeros
+        if ( oframes < frame_count ) {
+            memset((unsigned char *) out_data + oframes*stream->config.txElementSize,
+                   0,
+                   (frame_count - oframes)*stream->config.txElementSize);
         }
 
-        // No matter what happens, fill the remainder of the output buffer with zeros
-        if ( oframes < frame_count )
-            memset((unsigned char *) out_data + oframes*stream->txElementSize,
-                   0,
-                   (frame_count - oframes)*stream->txElementSize);
-
-        if ( oframes < frames_left) {
-            if ( frames < 0 ) {
-                // Now that we've reached the end of buffer, calculate our
-                // final frame count including padding and enter autoframes
-                // mode
-                if ( pad >= 0 ) {
-                    stream->_autoframes = 1;
-                    frames = stream->frames = stream->frame_count + oframes + pad;
-
-                    // exit point (2 of 2)
-                    // We don't want to do an unncessary callback; end here
-                    if ( stream->frame_count + frames_left >= frames ) {
-                        if ( stream->frame_count <= frames )
-                            frames_left = frames - stream->frame_count;
-                        else
-                            frames_left = 0;
-                        stream->last_callback = paComplete;
-                    }
-                }
-            }
-            else if ( pad < 0 ) { // Just pad out to stream->frames samples
-                stream->_autoframes = 1;
-            }
-            else if ( oframes < oframes_left ) {
-                strcpy(stream->errorMsg, "BufferEmpty");
-                stream->frame_count += oframes;
-                return stream->last_callback = paAbort;
+        if ( oframes < frames_left ) {
+            // strcpy(stream->errorMsg, "BufferEmpty");
+            if (!stream->config.keepAlive) {
+                stream->stats.frame_count += oframes;
+                return paAbort;
             }
         }
     }
@@ -187,67 +86,19 @@ int callback(
     }
 #endif
 
-    if ( stream->rxbuffer != NULL && stream->frame_count + frames_left > stream->offset ) {
-        if ( stream->frame_count < stream->offset ) {
-            offset = stream->offset - stream->frame_count;
-            frames_left -= offset;
-            in_data = (unsigned char *) in_data + offset*stream->rxElementSize;
-        }
-
-        if ( stream->rxmapping != NULL ) {
-            samplesize = stream->rxElementSize / stream->rxchannels;
-            memcpy(stream->_mapping, stream->rxmapping, stream->rxchannels);
-
-            for ( i = 0 ; i < stream->rxchannels ; i++ ) {
-                // inchannel = stream->_mapping[i];
-                // outchannel = i + 1;
-                if ( stream->_mapping[i] == i + 1 || stream->_mapping[i] == 0 )
-                    continue;
-
-                ptemp = (unsigned char *) in_data;
-                if ( i + 1 == stream->rxchannels || stream->_mapping[stream->_mapping[i] - 1] == 0 ) {
-                    for ( j = 0 ; j < frames_left ; j++, ptemp += stream->rxElementSize ) {
-                        memcpy(ptemp + i * samplesize, ptemp + (stream->_mapping[i] - 1)*samplesize, samplesize);
-                    }
-                }
-                else {
-                    for ( j = 0 ; j < frames_left ; j++, ptemp += stream->rxElementSize ) {
-                        for ( k = 0 ; k < samplesize ; k++) {
-                            temp = ptemp[i * samplesize + k];
-                            ptemp[i * samplesize + k] = ptemp[(stream->_mapping[i] - 1)*samplesize + k];
-                            ptemp[(stream->_mapping[i] - 1)*samplesize + k] = temp;
-                        }
-                    }
-                }
-
-                for ( k = i + 1 ; k < stream->rxchannels ; k++) {
-                    if (stream->_mapping[k] == i + 1)
-                        stream->_mapping[k] = stream->_mapping[i];
-                    else if (stream->_mapping[k] == stream->_mapping[i])
-                        stream->_mapping[k] = i + 1;
-                }
-            }
-            // Do channel zero out as a last step to avoid unnecessary swapping
-            for ( i = 0 ; i < stream->rxchannels ; i++ ) {
-                if ( stream->_mapping[i] != 0 )
-                    continue;
-
-                ptemp = (unsigned char *) in_data + i * samplesize;
-                for ( j = 0 ; j < frames_left ; j++, ptemp += stream->rxElementSize ) {
-                    memset(ptemp, 0, samplesize);
-                }
-            }
-        }
-
+    if ( stream->rxbuffer != NULL ) {
         iframes = PaUtil_WriteRingBuffer(stream->rxbuffer, (const void *) in_data, frames_left);
+        
         if ( iframes < frames_left ) {
-            strcpy(stream->errorMsg, "BufferFull");
-            stream->frame_count += iframes;
-            return stream->last_callback = paAbort;
+            // strcpy(stream->errorMsg, "BufferFull");
+            if (!stream->config.keepAlive) {
+                stream->stats.frame_count += iframes;
+                return paAbort;
+            }
         }
     }
 
     stream->lastTime = *timeInfo;
-    stream->frame_count += frame_count;
-    return stream->last_callback;
+    stream->stats.frame_count += frame_count;
+    return paContinue;
 }
--- src/py_pastream.h
+++ src/py_pastream.h
@@ -4,57 +4,29 @@
 // #define PYPA_DEBUG
 // #define PYPA_WIREMODE
 
-// interdependent behavior of frames & pad
-// frames < 0, pad >= 0:
-//   read until buffer is empty, then play 'pad' zero frames
-// frames >= 0, pad = X:
-//   read 'frames' frames, then play 'pad' zero frames
-// frames < 0, pad < 0:
-//   read any frames available and pad out the playback when there isn't enough
-//   to fill the buffer
-// TODO? get rid of 'frames' and exception re-raising
+typedef struct Py_PaStreamStats {
+    unsigned long frame_count;        // Number of frames successfully processed
+    unsigned xruns;
+    unsigned inputUnderflows;
+    unsigned outputUnderflows;
+    unsigned inputOverflows;
+    unsigned outputOverflows;
+} Py_PaStreamStats;
 
-typedef struct Py_PaStream {
-    PaStreamCallbackFlags status;
-    PaStreamCallbackTimeInfo lastTime;
-    int last_callback;
+typedef struct Py_PaStreamConfig {
     unsigned char loop;
-    unsigned long long frame_count;        // Number of frames successfully processed
-    long long frames;             // Number of frames to play/record (-1 means infinite)
-    long pad;                     // Number of zero frames to pad the playback with
-                                  // (< 0 means to pad playback whenever buffer is empty)
-    unsigned long offset;         // Number of frames to skip from beginning of recordings
-    unsigned char rxchannels, txchannels;
-    unsigned char *rxmapping, *txmapping, *_mapping;
-    unsigned long xruns;
-    ring_buffer_size_t txElementSize, rxElementSize;    
-    unsigned long inputOverflows, inputUnderflows;
-    unsigned long outputOverflows, outputUnderflows;
-    PaUtilRingBuffer *rxbuffer, *txbuffer;
-    char errorMsg[MAX_MESSAGE_LEN];  // Reserved for errors raised in the audio callback
-    unsigned char _autoframes;   // Internal use only
+    unsigned char keepAlive;
+    ring_buffer_size_t rxElementSize;    
+    ring_buffer_size_t txElementSize;
+} Py_PaStreamConfig;
+    
+typedef struct Py_PaStream {
+    PaUtilRingBuffer *rxbuffer;
+    PaUtilRingBuffer *txbuffer;
+    PaStreamCallbackTimeInfo lastTime;
+    Py_PaStreamStats stats;
+    Py_PaStreamConfig config;
 } Py_PaStream;
-
-// :sigh: Visual Studio doesn't support designated initializors
-const Py_PaStream Py_PaStream_default = {
-    0,
-    { 0 },
-    paContinue,
-    0,
-    0,
-    -1,
-    0,
-    0,
-    0, 0,
-    (unsigned char*) NULL, (unsigned char*) NULL, (unsigned char*) NULL,
-    0,
-    0, 0,
-    0, 0,
-    0, 0,
-    (PaUtilRingBuffer*) NULL, (PaUtilRingBuffer*) NULL,
-    { '\0' },
-    0
-};
 
 // Call once for initialization
 void init_stream(Py_PaStream *stream);
